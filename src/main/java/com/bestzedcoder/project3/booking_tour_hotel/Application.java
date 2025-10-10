6dd69343e4931dbe435332daf8563adbf022936e
bestzedcoder <quachlinh1252004@gmail.com>
2025-10-10 10:32:54 +0700

    setup env and folder project

--- src/main/java/com/bestzedcoder/project3/booking_tour_hotel/Application.java
+++ src/main/java/com/bestzedcoder/project3/booking_tour_hotel/Application.java
@@ -1,13 +1,22 @@
 package com.bestzedcoder.project3.booking_tour_hotel;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Value("${DATABASE_URL}")
+	private String databaseURL;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Database URL: " + databaseURL);
+	}
 }
