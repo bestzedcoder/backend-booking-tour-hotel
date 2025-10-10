0d4dc9452f878e1902316955bc7248ff3a0cd81d
bestzedcoder <quachlinh1252004@gmail.com>
2025-10-10 09:12:33 +0700

    setup project starter

---
+++ src/test/java/com/bestzedcoder/project3/booking_tour_hotel/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestzedcoder.project3.booking_tour_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
